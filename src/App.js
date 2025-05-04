41578c255a30b496b5c60e3d8a4c41ac5ed3cc27
Scott McIntoshi <206806438+McIntoshiS@users.noreply.github.com>
2025-05-03 23:20:20 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Taskbar from './components/Taskbar';
+import Desktop from './components/Desktop';
+import Window from './components/Window';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Desktop />
+      <Taskbar />
+      <Window title="My Computer">
+        <p>Welcome to Windows 95!</p>
+      </Window>
     </div>
   );
 }
